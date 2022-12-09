1acf67109444329d091e7dce78aa91662c912f93
unknown <mtharwat@onesolutionc.com>
2022-12-09 20:07:15 +0200

    add engpoint for creating and auth user

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options) :
         self.stdout.write('waiting for database..')
         db_conn = None
-        while not db_conn:
+        while not db_conn: 
             try:
                 db_conn = connections['default']
             except OperationalError:
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,7 +13,7 @@ class CommandTests(TestCase):
 
     @patch('time.sleep', return_value = True)
     def test_wait_for_db(self, ts):
-        """ test waiting for database"""
+        """ test waiting for database """
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OperationalError]*5+[True]
             call_command('wait_for_db')
